2f4abf3f2b0952c8dc999fabeed3c354cad5b55c
Jisoo.R <jisuryu0527@gmail.com>
2024-09-30 18:40:39 +0900

    Study: About Jackson

---
+++ Beginner/spring-mvc/src/main/java/com/sparta/springmvc/response/ResponseRestController.java
@@ -0,0 +1,23 @@
+package com.sparta.springmvc.response;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/response/")
+public class ResponseRestController {
+    // [Response header]
+    // Content - Type: text/html
+    // [Response body]
+    // {"name" : "Robbie", "age": 35}
+    @GetMapping("/json/string")
+    public String helloStringJson() {
+        return "{\"name\":\"Robbie\", \"age\":95}";
+    }
+
+    @GetMapping("/json/class")
+    public Star helloClassJson(){
+        return new Star("Robbie",95);
+    }
+}
